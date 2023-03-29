5d3412452d50dce8b0f6117d1dcc74b1a98fd3f5
Jesse Tomchak <jtomchak@gmail.com>
2023-03-29 11:03:52 -0700

    added session callback with jwt sync for client side git calls

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,6 +8,22 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
