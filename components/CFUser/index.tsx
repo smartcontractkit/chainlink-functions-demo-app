ba615261b4e169cb8a18d1e8ff0fb08b8f3a9c52
Jerry Hogan <jerry.hogan@thisdot.co>
2023-04-06 18:27:25 +0100

    in progress

--- components/CFUser/index.tsx
+++ components/CFUser/index.tsx
@@ -1,5 +1,12 @@
 import styles from "./CFUser.module.css";
 
-const CFUser = () => {};
+const CFUser = () => {
+
+  return (
+    <div className={styles.user_wrapper}>
+      <h4>Value</h4>
+    </div>
+  );
+};
 
 export default CFUser;
