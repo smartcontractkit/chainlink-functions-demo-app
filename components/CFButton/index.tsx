9d31abe5293fe052c29ad31270be3a21aa0f54f9
Jerry Hogan <jerry.hogan@thisdot.co>
2023-04-06 16:34:41 +0100

    button component done

--- components/CFButton/index.tsx
+++ components/CFButton/index.tsx
@@ -1,10 +1,21 @@
+import classNames from 'classnames';
 import styles from './CFButton.module.css';
+
 interface Props {
   text: string;
-  isDisabled: boolean;
+  isDisabled?: boolean;
+  size?: 'sm' | 'lg';
 }
-const CFButton = ({text, isDisabled }) => {
-  
+
+const CFButton = ({ text, size, isDisabled }: Props) => {
+  const btnClasses = classNames(styles.button_wrapper, {
+    [styles.button_sm_wrapper]: size === 'sm',
+  });
+  return (
+    <button className={btnClasses} disabled={isDisabled}>
+      {text}
+    </button>
+  );
 };
 
 export default CFButton;
