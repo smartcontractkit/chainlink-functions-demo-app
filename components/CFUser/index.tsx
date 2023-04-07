d5cec36ee34457f058f09444f13425be327e2d9b
Jerry Hogan <jerry.hogan@thisdot.co>
2023-04-07 07:44:29 +0100

    finalizing...

--- components/CFUser/index.tsx
+++ components/CFUser/index.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import styles from "./CFUser.module.css";
+import { useMetamask } from "hooks/useMetamask";
 
 const CFUser = () => {
+   const {
+     state: { wallet, balance },
+   } = useMetamask();
+  const slicedWallet = wallet?.slice(0,6) + '...' + wallet?.slice(-4);
+  const calcBalance = (parseInt(balance || '') / 1000000000000000000).toFixed(4);
 
   return (
     <div className={styles.user_wrapper}>
-      <h4>Value</h4>
+      <span>{calcBalance}</span>
+      <div className={styles.wallet_avatar}>
+        <span>{slicedWallet}</span>
+        <Image width={24} height={24} src="./icons/avatar.svg" alt="avatar" />
+      </div>
     </div>
   );
 };
