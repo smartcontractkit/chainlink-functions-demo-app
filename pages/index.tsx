08a5549630539c14e3dbb38ae59c349ea541ec10
Jerry Hogan <jerry.hogan@thisdot.co>
2023-04-11 11:54:16 +0100

    header component completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, Fragment } from 'react';
+import { useEffect, Fragment, useState } from 'react';
 import { useSession, signIn, signOut, getSession } from 'next-auth/react';
 import Wallet from '@components/Wallet';
 import Navbar from '@components/Navbar';
@@ -6,6 +6,8 @@ import { useListen } from '../hooks/useListen';
 import { useMetamask } from '../hooks/useMetamask';
 import { GetServerSidePropsContext } from 'next/types';
 import RepoForm from '@components/RepoForm';
+import CFOpenSourceMaintainer from '@components/CFOpenSourceMaintainer';
+import { Transition } from '@headlessui/react';
 
 interface IndexPageProps {
   user: User;
@@ -13,10 +15,13 @@ interface IndexPageProps {
 
 export default function IndexPage({ user }: IndexPageProps) {
   const { dispatch } = useMetamask();
+  const [isOpenM, setIsOpenM] = useState(false);
   const listen = useListen();
 
   useEffect(() => {
     if (typeof window !== undefined) {
+      const openM = Boolean(window.sessionStorage.getItem('openM'));
+      setIsOpenM(openM);
       // start by checking if window.ethereum is present, indicating a wallet extension
       const ethereumProviderInjected = typeof window.ethereum !== 'undefined';
       // this could be other wallets so we can verify if we are dealing with metamask
@@ -43,7 +48,20 @@ export default function IndexPage({ user }: IndexPageProps) {
 
   return (
     <>
-      <Navbar />
+      <Navbar isOpenM={isOpenM} />
+      <div className="w-full max-w-[1440px] px-4 lg:px-8">
+        <Transition
+          show={!isOpenM}
+          enter="transition ease-out duration-75"
+          enterFrom="transform opacity-0 -translate-y-full"
+          enterTo="transform opacity-100 translate-y-0"
+          leave="transition ease-in duration-100"
+          leaveFrom="transform opacity-100 translate-y-0"
+          leaveTo="transform opacity-0 -translate-y-full"
+        >
+          <CFOpenSourceMaintainer closeAlert={() => setIsOpenM(true)} />
+        </Transition>
+      </div>
       <Wallet />
       <RepoForm />
     </>
