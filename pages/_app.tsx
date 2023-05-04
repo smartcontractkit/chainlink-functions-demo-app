bc13632f8b4c1e6a920097558c3531c3ecf49861
Maarten Bicknese <maarten.bicknese@gmail.com>
2023-05-04 13:36:35 +0200

    feat: add GA scripts (#24)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,37 @@
 import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
 import { MetamaskProvider } from '../hooks/useMetamask';
 import { GithubRepoProvider } from '../hooks/useGithubRepo';
-
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ClaimProvider } from '../hooks/useClaim';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MetamaskProvider>
-      <ClaimProvider>
-        <GithubRepoProvider>
-          <SessionProvider session={pageProps.session} refetchInterval={0}>
-            <Component {...pageProps} />
-          </SessionProvider>
-        </GithubRepoProvider>
-      </ClaimProvider>
-    </MetamaskProvider>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-EGSL8E4K3V"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+        `}
+      </Script>
+      <MetamaskProvider>
+        <ClaimProvider>
+          <GithubRepoProvider>
+            <SessionProvider session={pageProps.session} refetchInterval={0}>
+              <Component {...pageProps} />
+            </SessionProvider>
+          </GithubRepoProvider>
+        </ClaimProvider>
+      </MetamaskProvider>
+    </>
   );
 }
